582a2a817dba31389948d280187fa9347e28379d
Sankalp Gour <sankalpgour123@gmail.com>
2017-08-30 04:59:52 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Prime {
+
+	public static void main(String[]args){
+
+		Scanner in=new Scanner(System.in);
+
+		System.out.print("Enter a Num: ");
+
+		int input=in.nextInt();
+
+		int mid=input/2;
+		
+int flag=0;
+
+		
+		/* for 2 to Mid of input Check the Mod operation of User input*/
+		
+		for(int i=2;i<mid;i++)
+		{
+
+			if(input%i==0)
+			{
+
+				flag=1;
+
+				break;
+
+			}
+
+		}
+		
+	if(flag==1)
+
+		System.out.println("The Num You Entered Is Not a Prime Number !!");
+
+	else
+ 
+		System.out.println("The Num You Entered Is a Prime Number !!");
+	
+	}
+
+}
