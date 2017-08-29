925edc4c3943473eabb476788c4b5e3e264b1168
Sankalp Gour <sankalpgour123@gmail.com>
2017-08-30 04:59:06 +0530

    Add files via upload

---
+++ Split.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Split{
+
+	public static void main(String[]args){
+
+		Scanner in=new Scanner(System.in);
+
+		String InputString="2.6, 5, 3.44, 6.88, 9";
+
+	        String [] arrOfStr = InputString.split(",");
+
+		double[]temp=new double[100];
+		/* count measure the length of temp array after spliting*/
+
+		int count=0;
+
+		for(String S:arrOfStr)
+			{
+
+			temp[i]=Double.parseDouble(a);
+			
+count++;
+
+			}
+
+		System.out.println("[ ");
+	
+		for(int j=0;j<count;j++)
+			{
+
+			System.out.println("  "+temp[j]);
+			
+}
+		System.out.print(" ]");	
+
+	}
+
+}
